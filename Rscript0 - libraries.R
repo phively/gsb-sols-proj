6dfeb5ef34caa8d6a59f2358202a8c8a6d6fcb90
phively <pbhively@gmail.com>
2016-04-05 19:14:00 -0500

    Add Rscript0 specifying libraries

---
+++ Rscript0 - libraries.R
@@ -0,0 +1,11 @@
+#### Useful packages to be used throughout ----
+# Joins and data wrangling
+library(dplyr)
+# Data reshaping
+library(tidyr)
+# Date formats
+library(lubridate)
+# Visualizations and beautification
+library(ggplot2)
+library(scales)
+library(knitr)
